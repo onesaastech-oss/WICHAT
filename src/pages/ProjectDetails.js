c0e751879c279b1123626aceec3f42fe9575c313
sourav1916 <souravadhikary1916@gmail.com>
2026-02-08 10:28:59 +0530

    Changed to old config

--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -355,8 +355,8 @@ const ProjectDetails = () => {
     };
 
     // Facebook Embedded Signup Configuration
-    const META_APP_ID = "665558946509856";
-    const META_CONFIG_ID = "1275572191131467";
+    const META_APP_ID = "4269135816705946";
+    const META_CONFIG_ID = "1432383791689294";
     const META_GRAPH_VER = "v24.0";
 
     // Initialize Facebook SDK
@@ -630,7 +630,7 @@ const ProjectDetails = () => {
                 override_default_response_type: true,
                 extras: {
                     setup: {
-                        solutionID: '1369066807875953'
+                        solutionID: '887791134017383'
                     },
                     featureType: "whatsapp_business_app_onboarding",
                     sessionInfoVersion: "3",
